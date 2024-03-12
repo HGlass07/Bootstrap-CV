9dac46f6e683f381b0e9e4bb0d47d37bafd72c73
Henry <hglasspool@googlemail.com>
2024-03-12 11:38:35 +0000

    fixed contact form submission

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,23 @@
-console.log("hellow")
+
+function sendMail() {
+    var templateParams = {
+        from_name: form.name.value,
+        from_email: form.email.value,
+        message: form.projectsummary.value,
+    };
+
+    emailjs.send('Henry', 'Inquiry', templateParams).then(
+        (response) => {
+            console.log('SUCCESS!', response.status, response.text);
+            $("form")[0].reset();
+        },
+        (error) => {
+            console.log('FAILED...', error);
+        },
+    );
+    return false;  // To block from loading a new page
+}
+
+
+
+
